b9f9f95002e04ce4c984e6b5bed52ef4c414f157
Adi Putra Pamungkas <adip14187@gmail.com>
2023-11-30 20:30:53 +0700

    upload file

---
+++ 3.py
@@ -0,0 +1,7 @@
+from math import pow
+
+pangkat_bilangan = pow(3,3)
+print("hasilnya adalah: ", pangkat_bilangan)
+
+#angka pertama adalah angka yang akan dipangkatkan
+#angka kedua adalah jumlah pemangkatan
